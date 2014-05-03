df886905c078f60b0297e0a043cb622718f61f5c
Ben A Morgan <ben@benmorgan.io>
2014-05-03 13:24:39 -0700

    set up vendor files

--- lib/generators/veddy/veddy_generator.rb
+++ lib/generators/veddy/veddy_generator.rb
@@ -1,9 +1,11 @@
 class VeddyGenerator < Rails::Generators::Base
   source_root File.expand_path("../templates", __FILE__)
-
   desc "This generator installs veddy."
-  def create_initializer_file
+
+  def create_vendor_files
     template "vendor/assets/javascripts/base64/base64-1.1.js"
+    template "vendor/assets/javascripts/base64/base64-1.1.min.js"
+    tmeplate "vendor/assets/javascripts/ved_analytics/ved_analytics-1.1.js"
+    template "vendor/assets/javascripts/ved_analytics/ved_analytics-1.1.min.js"
   end
 end
-
