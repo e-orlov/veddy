2e675b90e1ef7d3c4574a95ccebab79bba8cd7ed
Ben A Morgan <ben@benmorgan.io>
2014-05-03 13:15:52 -0700

    set to base64-1-1

--- lib/generators/veddy/veddy_generator.rb
+++ lib/generators/veddy/veddy_generator.rb
@@ -3,7 +3,7 @@ class VeddyGenerator < Rails::Generators::Base
 
   desc "This generator installs veddy."
   def create_initializer_file
-    template "vendor/assets/javascripts/base64/*"
+    template "vendor/assets/javascripts/base64/base64-1.1.js"
   end
 end
 
